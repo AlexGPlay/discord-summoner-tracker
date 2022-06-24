b4e0759add0db2425a2db860576ea6e1a6d1e670
Pelayo <pelayo.garcia97@gmail.com>
2022-06-24 17:22:41 +0200

    merge

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 const { setupDiscord } = require("./discord/setup");
-<<<<<<< HEAD
-
-// const { updateLocalSummonerInfo } = require('./riot/summoner');
-
-// (async () => {
-//   const summonerName = "Raistm";
-//   let events = await updateLocalSummonerInfo(summonerName);
-//   console.log(events);
-// })();
-
-setupDiscord();
-=======
 const { setupQueue: setupObserversQueue } = require("./queues/addObserverQueue");
 const { setupQueue: setupRemoveQueue } = require("./queues/removeObserverQueue");
 const { setupQueue: setupSummonerQueue } = require("./queues/trackSummonerQueue");
@@ -19,4 +7,3 @@ setupDiscord();
 setupObserversQueue();
 setupRemoveQueue();
 setupSummonerQueue();
->>>>>>> aa9b721689b6afd556c9329b21c86ea59d9d85d1

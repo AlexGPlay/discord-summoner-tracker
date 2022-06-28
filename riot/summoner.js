389e9b022ce330453a382309efbf08a7bace4598
a.garcia.parrondo <a.garcia@betterplaceapp.com>
2022-06-28 21:21:57 +0200

    Catch errors on summoner and return no events

--- riot/summoner.js
+++ riot/summoner.js
@@ -90,69 +90,74 @@ function removeObserver(summonerName, channelId) {
 }
 
 async function updateLocalSummonerInfo(summonerName) {
-  const summonerInfo = await getSummonerInfo(summonerName);
-  const localSummonerInfo = getLocalSummonerInfo(summonerName);
+  try {
+    const summonerInfo = await getSummonerInfo(summonerName);
+    const localSummonerInfo = getLocalSummonerInfo(summonerName);
 
-  if (!localSummonerInfo || Object.keys(localSummonerInfo).length < 2) {
-    saveInfoFromServer(summonerName, summonerInfo);
-    return [];
-  }
+    if (!localSummonerInfo || Object.keys(localSummonerInfo).length < 2) {
+      saveInfoFromServer(summonerName, summonerInfo);
+      return [];
+    }
 
-  let events = [];
+    let events = [];
 
-  const hasStartedPlaying = summonerInfo.isPlaying && !localSummonerInfo.isPlaying;
-  if (hasStartedPlaying) {
-    const participantInfo = summonerInfo.currentGameInfo.participants.find(
-      (participant) => participant.summonerName === summonerName
-    );
-
-    const championInfo = await getChampionById(participantInfo.championId);
-
-    events.push({
-      type: "start-playing",
-      summoner: summonerName,
-      participantInfo: { ...participantInfo, champion: championInfo },
-    });
-  }
-
-  summonerInfo.rank.forEach((rank) => {
-    const localRank = localSummonerInfo.rank.find(
-      (tmpRank) => tmpRank.queueType === rank.queueType
-    );
-    if (!localRank) return events.push({ type: "new-rank", summoner: summonerName, rank: rank });
-
-    if (localRank.tier === rank.tier && localRank.rank === rank.rank) return;
-
-    events.push({ type: "rank-change", summoner: summonerName, from: localRank, to: rank });
-  });
-
-  const alreadyPlayedGamesDifference = summonerInfo.lastGames.filter(
-    (game) => !localSummonerInfo.lastGames.includes(game)
-  ).reverse();
-  if (alreadyPlayedGamesDifference.length > 0) {
-    const requestedGames = await Promise.all(
-      alreadyPlayedGamesDifference.map((game) => getMatchInfoById(game))
-    );
-
-    for (const game of requestedGames) {
-      const summonerData = game.info.participants.find(
-        (participant) =>
-          participant.summonerName.trim().toLowerCase() === summonerName.trim().toLowerCase()
+    const hasStartedPlaying = summonerInfo.isPlaying && !localSummonerInfo.isPlaying;
+    if (hasStartedPlaying) {
+      const participantInfo = summonerInfo.currentGameInfo.participants.find(
+        (participant) => participant.summonerName === summonerName
       );
 
-      const championInfo = await getChampionById(summonerData.championId);
+      const championInfo = await getChampionById(participantInfo.championId);
 
       events.push({
-        type: "finished-playing",
+        type: "start-playing",
         summoner: summonerName,
-        matchId: game.metadata.matchId,
-        summonerData: { ...summonerData, champion: championInfo },
+        participantInfo: { ...participantInfo, champion: championInfo },
       });
     }
-  }
 
-  saveInfoFromServer(summonerName, summonerInfo);
-  return events;
+    summonerInfo.rank.forEach((rank) => {
+      const localRank = localSummonerInfo.rank.find(
+        (tmpRank) => tmpRank.queueType === rank.queueType
+      );
+      if (!localRank) return events.push({ type: "new-rank", summoner: summonerName, rank: rank });
+
+      if (localRank.tier === rank.tier && localRank.rank === rank.rank) return;
+
+      events.push({ type: "rank-change", summoner: summonerName, from: localRank, to: rank });
+    });
+
+    const alreadyPlayedGamesDifference = summonerInfo.lastGames
+      .filter((game) => !localSummonerInfo.lastGames.includes(game))
+      .reverse();
+    if (alreadyPlayedGamesDifference.length > 0) {
+      const requestedGames = await Promise.all(
+        alreadyPlayedGamesDifference.map((game) => getMatchInfoById(game))
+      );
+
+      for (const game of requestedGames) {
+        const summonerData = game.info.participants.find(
+          (participant) =>
+            participant.summonerName.trim().toLowerCase() === summonerName.trim().toLowerCase()
+        );
+
+        const championInfo = await getChampionById(summonerData.championId);
+
+        events.push({
+          type: "finished-playing",
+          summoner: summonerName,
+          matchId: game.metadata.matchId,
+          summonerData: { ...summonerData, champion: championInfo },
+        });
+      }
+    }
+
+    saveInfoFromServer(summonerName, summonerInfo);
+    return events;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 }
 
 module.exports = {
